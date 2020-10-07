c6079525a634fd2af66f53cbdd94df7445d29bf0
Valeriy Osipov <freebs@mail.ru>
2020-10-07 15:48:15 +0400

    init: start Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = "";
+  let close = "";
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open = open + bracketsConfig[i][0];
+    close = close + bracketsConfig[i][1];
+  }
+  let opened = "";
+  for (let j = 0; j < str.length; j++) {
+    if (open.indexOf(str[j]) !== -1) {
+      if ((close.indexOf(str[j]) !== -1) && (str[j] === opened[opened.length - 1])) {
+        opened = opened.slice(0, opened.length - 1);
+      } else {
+        opened = opened + str[j];
+      }
+    } else {
+      if ((opened[opened.length - 1] !== open[close.indexOf(str[j])]) || opened === "") {
+        return false;
+      } else {
+        opened = opened.slice(0, opened.length - 1);
+      }
+    }
+  }
+  if (opened === "") {
+    return true;
+  } else {
+    return false;
+  }
 }
